a6b7f07045a1d4815b1e67e1007919ea36ae6ede
Drathonix <52723768+Drathonix@users.noreply.github.com>
2024-10-02 18:41:54 -0400

    test2

---
+++ src/main/java/com/drathonix/nuclearearthmod/NEMod.java
@@ -0,0 +1,32 @@
+package com.drathonix.nuclearearthmod;
+
+import net.minecraftforge.fml.common.Loader;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = NEMod.MODID,
+        name = NEMod.NAME,
+        version = NEMod.VERSION,
+        dependencies = "after:industrialforegoing")
+public class NEMod
+{
+    public static final String MODID = "nuclearearthmod";
+    public static final String NAME = "Nuclear Earth Mod";
+    public static final String VERSION = "1.0.0";
+
+    private static Logger logger;
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        logger.info("Drathonix' Nuclear Earth Mod Initialized!");
+    }
+}
